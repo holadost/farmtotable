0aab24164b190b1acd45bc20fbcf24f8b42e562f
nikhil <nik.Gunner4life@gmail.com>
2020-08-21 22:18:00 -0700

    Initial commit.

---
+++ go/farmtotable/backend/constants.go
@@ -0,0 +1,9 @@
+package backend
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "farmToTable4u"
+	dbname   = "nikhil"
+)
---
+++ go/farmtotable/backend/postgres_backend_test.go
@@ -0,0 +1,24 @@
+package backend
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+func TestConnection(t *testing.T) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+}
