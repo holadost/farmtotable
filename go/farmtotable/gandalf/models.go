ae6fbc413f90fbbf52776d127b51eb555fa5bfa3
nikhil <nik.Gunner4life@gmail.com>
2020-09-10 17:31:01 -0700

    Added json tags to all models.

--- go/farmtotable/gandalf/models.go
+++ go/farmtotable/gandalf/models.go
@@ -11,43 +11,43 @@ type baseModel struct {
 
 /* Database model/schema */
 type User struct {
-	UserID  string `gorm:"type:varchar(100);PRIMARY_KEY"`
-	Name    string `gorm:"type:varchar(255);NOT NULL"`
-	EmailID string `gorm:"type:varchar(255);NOT NULL;index"`
-	PhNum   string `gorm:"type:varchar(20);NOT NULL;index"`
-	Address string `gorm:"NOT NULL"`
+	UserID  string `gorm:"type:varchar(100);PRIMARY_KEY" json:"user_id"`
+	Name    string `gorm:"type:varchar(255);NOT NULL" json:"name"`
+	EmailID string `gorm:"type:varchar(255);NOT NULL;index" json:"email_id"`
+	PhNum   string `gorm:"type:varchar(20);NOT NULL;index" json:"ph_num"`
+	Address string `gorm:"NOT NULL" json:"address"`
 }
 
 type Item struct {
-	ItemID           string `gorm:"type:varchar(32);PRIMARY_KEY"`
-	UserID           string `gorm:"type:varchar(100);index"`
-	ItemName         string `gorm:"type:varchar(255);NOT NULL"`
-	ItemDescription  string `gorm:"NOT NULL"`
-	ItemQty          uint32
-	AuctionStartTime time.Time
-	MinPrice         float32
+	ItemID           string    `gorm:"type:varchar(32);PRIMARY_KEY" json:"item_id"`
+	UserID           string    `gorm:"type:varchar(100);index" json:"user_id"`
+	ItemName         string    `gorm:"type:varchar(255);NOT NULL" json:"item_name"`
+	ItemDescription  string    `gorm:"NOT NULL" json:"item_description"`
+	ItemQty          uint32    `json:"item_qty"`
+	AuctionStartTime time.Time `json:"auction_start_time"`
+	MinPrice         float32   `json:"min_price"`
 }
 
 type Bid struct {
-	ItemID    string `gorm:"type:varchar(32);index"`
-	UserID    string `gorm:"type:varchar(100);index"`
-	BidAmount float32
-	BidQty    uint32
+	ItemID    string  `gorm:"type:varchar(32);index" json:"item_id"`
+	UserID    string  `gorm:"type:varchar(100);index" json:"user_id"`
+	BidAmount float32 `json:"bid_amount"`
+	BidQty    uint32  `json:"bid_qty"`
 }
 
 type Auction struct {
-	ItemID              string `gorm:"type:varchar(32);PRIMARY_KEY"`
-	ItemQty             uint32
-	AuctionStartTime    time.Time
-	AuctionDurationSecs uint64
-	MaxBid              float32
+	ItemID              string    `gorm:"type:varchar(32);PRIMARY_KEY" json:"item_id"`
+	ItemQty             uint32    `json:"item_qty"`
+	AuctionStartTime    time.Time `json:"auction_start_time"`
+	AuctionDurationSecs uint64    `json:"auction_duration_secs"`
+	MaxBid              float32   `json:"max_bid"`
 }
 
 type Order struct {
-	OrderID   string `gorm:"type:varchar(32);PRIMARY_KEY"`
-	UserID    string `gorm:"type:varchar(100);index"`
-	ItemID    string `gorm:"type:varchar(32);index"`
-	ItemQty   uint32
-	ItemPrice float32
-	Status    uint32
+	OrderID   string  `gorm:"type:varchar(32);PRIMARY_KEY" json:"order_id"`
+	UserID    string  `gorm:"type:varchar(100);index" json:"user_id"`
+	ItemID    string  `gorm:"type:varchar(32);index" json:"item_id"`
+	ItemQty   uint32  `json:"item_qty"`
+	ItemPrice float32 `json:"item_price"`
+	Status    uint32  `json:"status"`
 }
