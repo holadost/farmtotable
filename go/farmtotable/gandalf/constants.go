0624e21f05f96f175b61c696bd39022f134dea26
nikhil <nik.Gunner4life@gmail.com>
2020-11-27 15:49:38 -0800

    Added some types for order status.

--- go/farmtotable/gandalf/constants.go
+++ go/farmtotable/gandalf/constants.go
@@ -1,5 +1,10 @@
 package gandalf
 
+import (
+	"errors"
+	"fmt"
+)
+
 const (
 	PGHost     = "localhost"
 	PGPort     = 5432
@@ -12,13 +17,42 @@ const (
 	SQLiteDBPath = "/tmp/gandalf.db"
 )
 
+/* Order Status Codes */
 const (
-	KOrderPaymentPending     = 0
-	KOrderPaymentPendingStr  = "KOrderPaymentPending"
-	KOrderDeliveryPending    = 1
-	KOrderDeliveryPendingStr = "KOrderDeliveryPending"
-	KOrderComplete           = 2
-	KOrderCompleteStr        = "KOrderComplete"
-	KOrderCancelled          = 3
-	KOrderCancelledStr       = "KOrderCancelled"
+	KOrderPaymentPending  = 0
+	KOrderDeliveryPending = 1
+	KOrderComplete        = 2
+	KOrderCancelled       = 3
 )
+
+type OrderStatus uint32
+
+func (os OrderStatus) ToString() (OrderStatusStr, error) {
+	if os == KOrderPaymentPending {
+		return OrderStatusStr("KOrderPaymentPending"), nil
+	} else if os == KOrderDeliveryPending {
+		return OrderStatusStr("KOrderDeliveryPending"), nil
+	} else if os == KOrderComplete {
+		return OrderStatusStr("KOrderComplete"), nil
+	} else if os == KOrderCancelled {
+		return OrderStatusStr("KOrderCancelled"), nil
+	} else {
+		return OrderStatusStr("KUnsupportedStatus"), errors.New(fmt.Sprintf("unsupported order status: %d", os))
+	}
+}
+
+type OrderStatusStr string
+
+func (osr OrderStatusStr) ToUint32() (OrderStatus, error) {
+	if osr == "KOrderPaymentPending" {
+		return OrderStatus(KOrderPaymentPending), nil
+	} else if osr == "KOrderDeliveryPending" {
+		return OrderStatus(KOrderDeliveryPending), nil
+	} else if osr == "KOrderComplete" {
+		return OrderStatus(KOrderComplete), nil
+	} else if osr == "KOrderCancelled" {
+		return OrderStatus(KOrderCancelled), nil
+	} else {
+		return 9999999, errors.New(fmt.Sprintf("unsupported error status: %s", osr))
+	}
+}
