25f07bd70becec8189e440bf4c307a5859af7c63
nikhil <nik.Gunner4life@gmail.com>
2021-01-08 01:03:26 -0800

    Completed the register bid widget. Now need to do the actual API and check if the bid gets registered.

--- dart/farmtotable/frodo/lib/widgets/register_bid_widget.dart
+++ dart/farmtotable/frodo/lib/widgets/register_bid_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class RegisterBidWidget extends StatefulWidget {
-
   @override
   _RegisterBidWidgetState createState() => _RegisterBidWidgetState();
 }
@@ -9,52 +8,59 @@ class RegisterBidWidget extends StatefulWidget {
 class _RegisterBidWidgetState extends State<RegisterBidWidget> {
   final _qtyController = TextEditingController();
   final _amountController = TextEditingController();
+  bool _isBeingSubmitted = false;
 
-  void _submitData() {
+  void _submitData() async {
+    if (_qtyController.text == "" || _amountController.text == "") {
+      print("error. Did not submit data");
+      return;
+    }
     final qty = _qtyController.text;
     final amount = double.parse(_amountController.text);
-    print("Quantity: $qty, Price: $amount");
+    setState(() {
+      _isBeingSubmitted = true;
+    });
     Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Card(
-          elevation: 5,
-          child: Container(
-            padding: EdgeInsets.only(
-                top: 10,
-                left: 10,
-                right: 10,
-                bottom: MediaQuery.of(context).viewInsets.bottom + 10),
-            child:Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Quantity"
+    return _isBeingSubmitted
+        ? CircularProgressIndicator()
+        : SingleChildScrollView(
+            child: Card(
+                elevation: 5,
+                child: Container(
+                  padding: EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                      right: 10,
+                      bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(labelText: "Quantity"),
+                        controller: _qtyController,
+                        keyboardType: TextInputType.number,
+                        onSubmitted: (_) => _submitData(),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: "Amount"),
+                        controller: _amountController,
+                        keyboardType: TextInputType.number,
+                        onSubmitted: (_) => _submitData(),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: _submitData,
+                        child: const Text("Bid now!"),
+                      )
+                    ],
                   ),
-                  controller: _qtyController,
-                  keyboardType: TextInputType.number,
-                  onSubmitted: (_) => _submitData(),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Amount"
-                  ),
-                  controller: _amountController,
-                  keyboardType: TextInputType.number,
-                  onSubmitted: (_) => _submitData(),
-                ),
-                ElevatedButton(
-                  onPressed: _submitData,
-                  child: const Text("Bid now!"),
-                )
-              ],
-            ),
-          )
-      ),
-    );
+                )),
+          );
   }
 }
