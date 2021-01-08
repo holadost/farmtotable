e3b5531fc12c98d08172d8728111d523c301e22d
nikhil <nik.Gunner4life@gmail.com>
2021-01-08 15:01:11 -0800

    Removed the test only add auctions API.

--- go/farmtotable/aragorn/aragorn.go
+++ go/farmtotable/aragorn/aragorn.go
@@ -84,6 +84,7 @@ func (aragorn *Aragorn) Run() {
 	r.POST("/api/v1/resources/orders/get_delivery_pending_orders", aragorn.GetDeliveryPendingOrders) // Administrator API.
 	r.POST("/api/v1/resources/orders/update_order", aragorn.UpdateOrder)                             // Administrator API.
 	r.POST("/api/v1/resources/orders/purchase", aragorn.PurchaseOrder)                               // UserModel API.
+	r.POST("/api/v1/resources/test/orders/test_only_add_order", aragorn.TestOnlyAddOrder)            // Test API.
 
 	// Start router.
 	r.Run(":8080")
@@ -679,6 +680,42 @@ func (aragorn *Aragorn) UpdateOrder(c *gin.Context) {
 
 func (aragorn *Aragorn) PurchaseOrder(c *gin.Context) {
 	// TODO: Still needs to be implemented
+	return
+}
+
+func (aragorn *Aragorn) TestOnlyAddOrder(c *gin.Context) {
+	// This is a test API. This must not be used for any other reason.
+	var ret TestOnlyAddOrderRet
+	var arg TestOnlyAddOrderArg
+	if err := c.ShouldBindJSON(&arg); err != nil {
+		ret.Status = http.StatusBadRequest
+		ret.ErrorMsg = "Invalid input JSON"
+		c.JSON(http.StatusBadRequest, ret)
+		aragorn.apiLogger.Error(fmt.Sprintf("%s: error: %v", ret.ErrorMsg, err))
+		return
+	}
+	var orders []gandalf.OrderModel
+	var order gandalf.OrderModel
+	order.ItemID = arg.ItemID
+	order.UserID = arg.UserID
+	order.ItemPrice = arg.ItemPrice
+	order.ItemQty = arg.ItemQty
+	orders = append(orders, order)
+	err := aragorn.gandalf.AddOrders(orders)
+	if err != nil {
+		ret.Status = http.StatusInternalServerError
+		ret.ErrorMsg = "Unable to add order"
+		c.JSON(http.StatusInternalServerError, ret)
+		aragorn.apiLogger.Error(fmt.Sprintf("%s: error: %v", ret.ErrorMsg, err))
+		return
+	}
+	ret.Status = http.StatusOK
+	ret.ErrorMsg = ""
+	retData := RegistrationStatusRet{
+		RegistrationStatus: true,
+	}
+	ret.Data = retData
+	c.JSON(http.StatusOK, ret)
 	return
 }
 
