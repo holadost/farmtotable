b6f609d9b88c536489346f8b63995138e4e5158f
nikhil <nik.Gunner4life@gmail.com>
2021-01-08 19:23:03 -0800

    Able to successfully register bids. Better user alert messages as well.

--- dart/farmtotable/frodo/lib/widgets/register_bid_widget.dart
+++ dart/farmtotable/frodo/lib/widgets/register_bid_widget.dart
@@ -18,44 +18,104 @@ class _RegisterBidWidgetState extends State<RegisterBidWidget> {
   bool _isBeingSubmitted = false;
   var apiClient = RestApiClient();
 
+  void _showAlert(String title, String content) {
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              title: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.red),
+              ),
+              content: Text(content),
+              actions: [
+                FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("OK"))
+              ],
+            ));
+  }
+
   void _submitData() async {
     if (_qtyController.text == "" || _amountController.text == "") {
-      print("error. Did not submit data");
+      _showAlert("Invalid inputs", "Enter an amount and quantity");
       return;
     }
-    final qty = int.parse(_qtyController.text);
-    final amount = double.parse(_amountController.text);
+
+    // Check quantity.
+    int qty;
+    try {
+      qty = int.parse(_qtyController.text);
+    } catch (error) {
+      _showAlert("Invalid quanity", "Quantity must be a whole number");
+      return;
+    }
+    if (qty < widget.item.minBidQty ||
+        qty > widget.item.maxBidQty ||
+        qty > widget.item.itemQty ||
+        qty % widget.item.minBidQty != 0) {
+      _showAlert(
+          "Invalid bid quantity",
+          "The bid quantity must be >= than "
+              "${widget.item.minBidQty}${widget.item.itemUnit} "
+              "and must be lesser than ${widget.item.maxBidQty}${widget.item.itemUnit}"
+              "and must be a multiple of ${widget.item.minBidQty}${widget.item.itemUnit}");
+      return;
+    }
+
+    if (qty < widget.item.minBidQty ||
+        qty > widget.item.maxBidQty ||
+        qty > widget.item.itemQty ||
+        qty % widget.item.minBidQty != 0) {
+      _showAlert(
+          "Invalid bid quantity",
+          "The bid quantity must be >= than "
+              "${widget.item.minBidQty}${widget.item.itemUnit} "
+              "and must be lesser than ${widget.item.maxBidQty}${widget.item.itemUnit}"
+              "and must be a multiple of ${widget.item.minBidQty}${widget.item.itemUnit}");
+      return;
+    }
+
+    // Check amount.
+    double amount;
+    try {
+      amount = double.parse(_amountController.text);
+    } catch (error) {
+      _showAlert("Invalid amount", "Amount must be a non-zero number");
+      return;
+    }
+    if ((qty <= 0) || (amount <= 0)) {
+      _showAlert("Invalid amount/quantity", "Quantity and Amount must be > 0");
+      return;
+    }
+
+    // Set state so that user can see a loading spinner while we process
+    // the request.
     setState(() {
       _isBeingSubmitted = true;
     });
+
     try {
       await apiClient.registerBid(widget.item.itemID, amount, qty);
       Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text("Registered bid successfully"),
+        content: Text(
+          "Registered bid successfully",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
         duration: Duration(seconds: 2),
         backgroundColor: Colors.green,
       ));
     } catch (error) {
-      showDialog(
-          context: context,
-          builder: (ctx) => AlertDialog(
-                title: Text(
-                  "Bid failed",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.red),
-                ),
-                content: Text(
-                  "The bid was invalid. Ensure that the current bid is greater than your previous bid",
-                ),
-                actions: [
-                  FlatButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text("OK"))
-                ],
-              ));
+      _showAlert(
+          "Bid failed",
+          "The bid was invalid. Ensure that the current bid is greater than "
+              "your previous bid");
     }
+    // Set state so that we can disable the loading spinner and user can
+    // see the response.
     setState(() {
       _isBeingSubmitted = false;
     });
@@ -78,6 +138,16 @@ class _RegisterBidWidgetState extends State<RegisterBidWidget> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
+                      Container(
+                          height: 250,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              image: DecorationImage(
+                                  fit: BoxFit.fill, image: NetworkImage(widget.item.imageURL)))),
+                      SizedBox(
+                        height: 20,
+                      ),
                       TextField(
                         decoration: InputDecoration(labelText: "Quantity"),
                         controller: _qtyController,
