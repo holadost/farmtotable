389b1abf9d3f93d8fa0b448dfe6e44c6ae6cb3f3
nikhil <nik.Gunner4life@gmail.com>
2021-01-11 19:08:39 -0800

    Added a lib to quickly print out some log messages.

---
+++ dart/farmtotable/frodo/lib/util/logging.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+
+void info(String message) {
+  log("INFO", message);
+}
+
+void error(String message) {
+  log("ERROR", message);
+}
+
+void warning(String message) {
+  log("WARNING", message);
+}
+
+void log(String level, String message) {
+  var now = new DateTime.now().toUtc().toIso8601String();
+  stdout.writeln("$now [$level] $message\n");
+}
