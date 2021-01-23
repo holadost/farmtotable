5ee3b9d001d2b8ed3de6da85b78bc449f0bd203b
nikhil <nik.Gunner4life@gmail.com>
2021-01-22 18:13:41 -0800

    Able to sign up and login a user. Need to handle error scenarios.

--- dart/farmtotable/frodo/lib/providers/auth_provider.dart
+++ dart/farmtotable/frodo/lib/providers/auth_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/intl.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../util/logging.dart';
@@ -8,28 +8,55 @@ import '../util/logging.dart';
 class AuthProvider with ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   String _idToken = "";
-  String _userID = "";
-  String _userName = "";
-  String _userEmailAddress = "";
+  DateTime _expTime;
 
   String get token {
     return _idToken;
   }
 
-  Future<bool> login(String userEmail, String password) async {
-    info("Successfully logged in");
-    notifyListeners();
+  bool isAuthorized() {
+    if (_idToken == "" || _idToken == null) {
+      return false;
+    }
+    if (_expTime == null || _expTime.isBefore(DateTime.now())) {
+      return false;
+    }
     return true;
+  }
+
+  Future<bool> login(String userEmail, String password) async {
+    try {
+      info("Logging in user: $userEmail");
+      final result = await _auth.signInWithEmailAndPassword(
+          email: userEmail, password: password);
+      final user = _auth.currentUser;
+      final tokenRes = await user.getIdTokenResult(true);
+      _idToken = tokenRes.token;
+      _expTime = tokenRes.expirationTime;
+      notifyListeners();
+      info("Successfully logged in. User ID: ${user.uid}."
+          "Expiry Time: ${DateFormat.yMMMMd().add_jm().format(_expTime)}");
+      return true;
+    } catch (error) {
+      info("Caught error while logging in: ${error.toString()}");
+      return false;
+    }
   }
 
   Future<bool> signup(String userEmail, String password) async {
     try {
-      final result = await _auth.createUserWithEmailAndPassword(
+      info("Signing up user: $userEmail");
+      await _auth.createUserWithEmailAndPassword(
           email: userEmail, password: password);
-      info("Signed up!");
+      info("Successfully signed up new user! UID: "
+           "${_auth.currentUser.uid}");
+      final res = await login(userEmail, password);
+      if (!res) {
+        return false;
+      }
       return true;
     } catch (error) {
-      info("Caught error: ${error.toString()}");
+      info("Caught error while signing up: ${error.toString()}");
      return false;
     }
   }
